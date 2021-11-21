e480eea7a9fdc14e4e49de62821931199590b2b5
kiritjmodi <kiritmodi@gmail.com>
2021-11-21 20:22:13 +0530

    Update database config

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos! #{DateTime.now.to_s(:short)}"
+   # render plain:"Hello, this is /todos! #{DateTime.now.to_s(:short)}"
+    render plain: Todo.order(:due_date).map { |todo| todo.todo_text}.join("\n")
   end
 end
